24ae38af30753d0053b62dd5df170202699caef1
Kaung Khant Kyaw <kcubez21@gmail.com>
2025-05-05 15:33:08 +0700

    Database url to env file

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL =
-  'mongodb+srv://root:1234@books-store-mern.5yxrscv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN';
+export const mongoDBURL = process.env.MONGODB_URL;
